79e6857b96de6b3a2b356a21eec0de302cc14b1e
Chathura <c.rathnayake97@gmail.com>
2021-05-29 22:19:13 +0530

    Added source codes related to PHP basics

---
+++ PHP Basics/post.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="post_output.php">
+  Enter you first name: <input type="text" name="fname">
+  <input type="submit" value="send" name="send">
+</form>
+
+</body>
+</html>
